0cbc4ae4468da4cadcb652277c92dcfc74f6e350
Srini Nariangadu <srini.nariangadu@gmail.com>
2021-01-13 18:07:59 -0800

    Initial commit of package with 2 implemented functions

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # lambdata
+This is a package that is being created as part of my coursework for Unit 3 of Lambda School's Data Science program.
+
+To use 
+`from lambdata_nsriniva import helper_functions`
+
+The `helper_functions` module implements:
+`null_count(df)`: Check a dataframe for nulls and return the number of missing values.
+				returns null count integer
+
+and
+
+`abbr_2_st(state_series, abbr_2_st=True)`: Return a new column with the full name from a State abbreviation column -> An input of FL would return Florida. This function should also take a boolean (abbr_2_state) and when False takes full state names and return state abbreviations. -> An input of Florida would return Fl.
+										returns pd.Dataframe with translations
---
+++ lambdata-nsriniva/helper_functions.py
@@ -0,0 +1,38 @@
+from os import name
+from pandas import DataFrame, Series
+from numpy import NaN
+from us_state_abbrev import us_state_abbrev, abbrev_us_state
+
+def null_count(df):
+    print(f'Input DataFrame:\n{df}\n')
+
+    return df.isna().sum().sum()
+
+ST_ABBR = ('st','abbr')
+
+def abbr_2_st(state_series, abbr_2_st=True):
+    ret_list = list()
+
+    opers = ((us_state_abbrev, 'name'),(abbrev_us_state, 'abbreviation'))
+    
+    oper = int(abbr_2_st)
+    
+    print(f'Input Series:\n{state_series}\n')
+    for elem in state_series:
+        ret_list.append(opers[oper][0].get(elem,f'{elem} is not a valid state {opers[oper][1]}'))
+    
+    states_df = DataFrame()
+    states_df[ST_ABBR[oper]] = state_series  
+    states_df[ST_ABBR[1-oper]] = Series(ret_list)
+    return states_df
+
+if __name__ == "__main__":
+    Null_DF = DataFrame({'col0':[NaN, 4, 3],'col1':[9,NaN, NaN], 'col2':[10, 2, 2]})
+    abbr_st = DataFrame({ST_ABBR[1]:['AL', 'AZ', 'CA', 'DE', 'OH'], ST_ABBR[0]:['Alabama','Arizona','California','Delaware','Ohio']})
+
+    print(Null_DF)
+    print(abbr_st)
+
+    print(f'Null Count : {null_count(Null_DF)}\n')
+    print(f'Output DataFrame:\n{abbr_2_st(abbr_st[ST_ABBR[1]])}\n')
+    print(f'Output DataFrame:\n{abbr_2_st(abbr_st[ST_ABBR[0]], False)}\n')
