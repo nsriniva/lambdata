2c63765686a9fb4e4630706e160c1ae25ec14585
Srini Nariangadu <srini.nariangadu@gmail.com>
2021-01-13 18:21:08 -0800

    Modified abbr_2_st() to return a Series object

--- README.md
+++ README.md
@@ -6,9 +6,13 @@ To use
 
 The `helper_functions` module implements:
 `null_count(df)`: Check a dataframe for nulls and return the number of missing values.
+
 				returns null count integer
 
 and
 
-`abbr_2_st(state_series, abbr_2_st=True)`: Return a new column with the full name from a State abbreviation column -> An input of FL would return Florida. This function should also take a boolean (abbr_2_state) and when False takes full state names and return state abbreviations. -> An input of Florida would return Fl.
-										returns pd.Dataframe with translations
+`abbr_2_st(state_series, abbr_2_st=True)`: Return a new column containing the full name from a State abbreviation column 
+											-> An input of FL would return Florida. This function should also take a boolean (abbr_2_state) and when False takes full state names and return state abbreviations. 
+											-> An input of Florida would return Fl.
+
+										returns pd.Series with translations
--- lambdata-nsriniva/helper_functions.py
+++ lambdata-nsriniva/helper_functions.py
@@ -21,10 +21,13 @@ def abbr_2_st(state_series, abbr_2_st=True):
     for elem in state_series:
         ret_list.append(opers[oper][0].get(elem,f'{elem} is not a valid state {opers[oper][1]}'))
     
-    states_df = DataFrame()
-    states_df[ST_ABBR[oper]] = state_series  
-    states_df[ST_ABBR[1-oper]] = Series(ret_list)
-    return states_df
+    #states_df = DataFrame()
+    #states_df[ST_ABBR[oper]] = state_series  
+    #states_df[ST_ABBR[1-oper]] = Series(ret_list)
+
+    ret_series = Series({ST_ABBR[1-oper]:ret_list})
+
+    return ret_series
 
 if __name__ == "__main__":
     Null_DF = DataFrame({'col0':[NaN, 4, 3],'col1':[9,NaN, NaN], 'col2':[10, 2, 2]})
@@ -34,5 +37,5 @@ if __name__ == "__main__":
     print(abbr_st)
 
     print(f'Null Count : {null_count(Null_DF)}\n')
-    print(f'Output DataFrame:\n{abbr_2_st(abbr_st[ST_ABBR[1]])}\n')
-    print(f'Output DataFrame:\n{abbr_2_st(abbr_st[ST_ABBR[0]], False)}\n')
+    print(f'Output Series:\n{abbr_2_st(abbr_st[ST_ABBR[1]])}\n')
+    print(f'Output Series:\n{abbr_2_st(abbr_st[ST_ABBR[0]], False)}\n')
