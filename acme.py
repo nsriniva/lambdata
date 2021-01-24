397a17caf46ec027fd85623d37829341eb0e225c
Srini Nariangadu <srini.nariangadu@gmail.com>
2021-01-24 00:48:57 -0800

    Added method docstrings to acme.py

--- acme.py
+++ acme.py
@@ -11,6 +11,12 @@ class Product():
         self.identifier = randint(1000000, 9999999)
 
     def stealability(self):
+        """
+        Calculates the price divided by the weight, and then returns a message: 
+        if the ratio is less than 0.5 return "Not so stealable...", if it is 
+        greater or equal to 0.5 but less than 1.0 return "Kinda stealable.", and
+        otherwise return "Very stealable!"
+        """
         val = self.price/self.weight
 
         if val < 0.5:
@@ -23,6 +29,12 @@ class Product():
         return ret
 
     def explode(self):
+        """
+        Calculates the flammability times the weight, and then returns a message:
+        if the product is less than 10 return "...fizzle.", if it is greater or 
+        equal to 10 but less than 50 return "...boom!", and otherwise return 
+        "...BABOOM!!"
+        """
         val = self.flammability * self.weight
 
         if val < 10:
@@ -44,6 +56,11 @@ class BoxingGlove(Product):
         return "...it's a glove."
 
     def punch(self):
+        """
+        This method returns "That tickles." if the weight is below 5, 
+        "Hey that hurt!" if the weight is greater or equal to 5 but 
+        less than 15, and "OUCH!" otherwise
+        """
         val = self.weight
 
         if val < 5:
